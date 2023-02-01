c7ccef5a13dd9b9ba255b8faaaf9985bcaccc79e
Winfieldpaul <paul.fize@gmail.com>
2023-02-02 08:03:08 +0900

    toppage

--- js/header.js
+++ js/header.js
@@ -1,6 +1,6 @@
 $(function () {
   $(window).on('scroll', function () {
-    if ($('.header').height()  < $(this).scrollTop()) {
+    if ($('.onepx').height()  < $(this).scrollTop()) {
         $('.js-header').addClass('change-color');
     } else {
         $('.js-header').removeClass('change-color');
